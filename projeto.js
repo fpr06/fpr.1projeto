c632a37286662cef6816ef4b11197caae67c091d
fpr06 <128843401+fpr06@users.noreply.github.com>
2023-12-04 22:07:13 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,28 @@
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+  }
+   
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+  }
+  
+  // Modal Image Gallery
+  function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+  }
+  
+  function inscrever() {
+  document.getElementById("btn-escreverse").innerHTML = "Inscrito";
+}
+function inscreve() {
+  document.getElementById("bt-escrevere").innerHTML = "Inscrito";
+}
+
+  
+
